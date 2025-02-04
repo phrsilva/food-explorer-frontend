21e5313c52b20314a774e9fe9beb5dba021fdf4d
megafisto <phrsilva.adv@gmail.com>
2025-02-03 22:59:07 -0300

    função de sair da conta no menu

--- src/componentes/menuLateral/index.jsx
+++ src/componentes/menuLateral/index.jsx
@@ -2,17 +2,26 @@ import {Container, Cabecalho, Corpo, RodapeMenu} from "./styles"
 import { FiX } from "react-icons/fi"
 import {Entrada} from "../../componentes/entrada"
 import { BotaoDeTexto } from "../botaoDeTexto"
+import { useNavigate } from "react-router-dom";
+import { usarAutenticacao } from "../../hooks/aut";
 
 export function MenuLateral({fecharMenu}) {
+    const navigate = useNavigate();
+    const { sair } = usarAutenticacao();
+
+    function sairDaConta() {
+        sair()
+        navigate("/")
+    }
     return (
         <Container>
             <Cabecalho>
-                <FiX onClick={fecharMenu}/>
+                <FiX onClick={fecharMenu} />
                 <h1>menu</h1>
             </Cabecalho>
             <Corpo>
                <Entrada type="text" placeholder="Busque por pratos ou ingredientes"/>
-               <BotaoDeTexto title="Sair" />
+               <BotaoDeTexto title="Sair" onClick={sairDaConta}/>
                <span></span>
             </Corpo>
             <RodapeMenu/>

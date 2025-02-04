cae03ad899e8fe16a734acf23d31fe8b121d5cea
megafisto <phrsilva.adv@gmail.com>
2025-02-04 00:52:25 -0300

    logo da barra de navegacao

--- src/componentes/logo/index.jsx
+++ src/componentes/logo/index.jsx
@@ -4,18 +4,18 @@ import {useEffect, useState} from "react";
 import { usarAutenticacao } from "../../hooks/aut";
 
 export function Logo({className}) {
-    // const {usuario} = usarAutenticacao();
+    const {usuario} = usarAutenticacao();
 
 
     return (
         <Container className={className}>
             <img src={logoImg} alt="Logo" />
             <h1>food explorer</h1>
-            {/* {
+            {
                 usuario.perfil === 'admin' &&
                 <p>Admin</p>
             }
-             */}
+            
         </Container>
     )
 }
